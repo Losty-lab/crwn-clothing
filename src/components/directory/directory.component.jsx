cdac7d6c9c9457c54eb017e97f90dddd9d81d8a3
Dario Lostumbo <dario.lostumbo@hotmail.com>
2019-09-12 01:10:18 +0200

    Router

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,19 +14,16 @@ class Directory extends React.Component{
             sections:[{
                 title:'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                
                 id:1
             },
             {
                 title:'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                
                 id:2
             },
             {
                 title:'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                
                 id:3
             },
             {
